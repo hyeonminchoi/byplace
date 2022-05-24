f6645da3cdc9947bb5750ddab7ccbd6e38c456f3
82102 <82102@DESKTOP-D9SC9KT>
2022-05-24 16:23:28 +0900

    커밋

---
+++ src/main/java/com/byplace/web/BoardWrite.java
@@ -0,0 +1,52 @@
+package com.byplace.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.byplace.dao.BoardDAO;
+import com.byplace.dto.BoardDTO;
+import com.byplace.dto.UserDTO;
+
+@WebServlet("/boardWrite")
+public class BoardWrite extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public BoardWrite() {
+        super();
+
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("USER") !=null) {
+			response.sendRedirect("./boardWrite.jsp");
+		} else { // 로그인이 안 됐을경우
+			response.sendRedirect("./index.jsp");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("USER") !=null) {
+			if(request.getParameter("content") !=null && request.getParameter("title") !=null ) {
+				BoardDTO dto = new BoardDTO();
+				dto.setBoard_title(request.getParameter("title"));
+				dto.setBoard_comment(request.getParameter("content"));
+				dto.setUser_no(((UserDTO)session.getAttribute("USER")).getUser_no());
+				BoardDAO dao = new BoardDAO();
+				dao.boardwrite(dto);
+			} else { // 제목 or 내용이 없을 경우
+				response.sendRedirect("./index.jsp");
+			}
+		} else { // 로그인이 안 됐을경우
+			response.sendRedirect("./index.jsp");
+		}
+	}
+
+}
---
+++ src/main/java/com/byplace/web/Detail.java
@@ -0,0 +1,53 @@
+package com.byplace.web;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.byplace.dao.BoardDAO;
+import com.byplace.dto.BoardDTO;
+import com.byplace.dto.BoardcommentDTO;
+import com.byplace.util.Util;
+
+@WebServlet("/detail")
+public class Detail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Detail() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (request.getParameter("board_no") != null && Util.str2Int(request.getParameter("board_no"))) {
+			long board_no = Integer.parseInt(request.getParameter("board_no"));
+			BoardDAO dao = new BoardDAO();
+			dao.countUp(board_no);
+			BoardDTO detail = dao.detail(board_no);
+			if (detail.getBoard_commentcount() > 0) {
+				List<BoardcommentDTO> commList = dao.commentList(board_no);
+				request.setAttribute("commList", commList);
+			}
+
+			RequestDispatcher rd = request.getRequestDispatcher("/detail.jsp");
+			request.setAttribute("detail", detail);
+			rd.forward(request, response);
+
+		} else {
+			response.sendRedirect("./board");
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
